ec7bf95af9d9a4eca5af975e18016566d553fcfb
dante8888inferno <123454385+dante8888inferno@users.noreply.github.com>
2023-10-11 16:54:24 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,6 @@
 
 set -eu
 
-export GITHUB="true"
+[ -n "$INPUT_STRIP_COMPONENTS" ] && export INPUT_STRIP_COMPONENTS=$((INPUT_STRIP_COMPONENTS + 0))
 
-sh -c "/bin/drone-ssh $*"
+sh -c "/bin/drone-scp $*"
